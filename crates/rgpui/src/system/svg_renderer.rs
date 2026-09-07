a034fd63a55a082cbe409f56c7660249fcc5d6ed
deego <deego@deego.com>
2026-09-07 09:40:28 +0800

    fix: 修复 svg_renderer.rs 测试模块缩进（cargo fmt）

--- crates/rgpui/src/system/svg_renderer.rs
+++ crates/rgpui/src/system/svg_renderer.rs
@@ -308,7 +308,7 @@ mod tests {
 
     const IBM_PLEX_REGULAR: &[u8] =
         include_bytes!("../../../../assets/fonts/ibm-plex-sans/IBMPlexSans-Regular.ttf");
-        const LILEX_REGULAR: &[u8] = include_bytes!("../../../../assets/fonts/lilex/Lilex-Regular.ttf");
+    const LILEX_REGULAR: &[u8] = include_bytes!("../../../../assets/fonts/lilex/Lilex-Regular.ttf");
 
     fn db_with_bundled_fonts() -> Database {
         let mut db = Database::new();
